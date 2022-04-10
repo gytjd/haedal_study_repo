64bc00cf8e5fbd18de4d0e97a87bf93677aaaf1f
jiwon-dev <jiwon9902@naver.com>
2022-04-10 23:48:10 +0900

    Delete 1이 될 때까지.py

--- 1차시/1이 될 때까지.py
+++
@@ -1,9 +0,0 @@
-import sys
-input = sys.stdin.readline
-N, K = map(int, input().split())
-ans = 0
-while True:
-    if N == 1: print(ans); break
-    if N % K == 0: N /= K
-    else: N -= 1
-    ans += 1
